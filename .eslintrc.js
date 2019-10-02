eeb8c171ce068e0e1dcd031163105d45155a6373
Alan Nunes <alanunesouza@hotmail.com>
2019-10-02 00:17:10 -0300

    instalacao e config redux e layout home e cart

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,11 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-    'react-hooks'
-  ],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
@@ -32,10 +28,14 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
     'react/prefer-stateless-function': 'off',
-    'no-console': ["error", {
-      allow: ["tron"]
-    }],
+    'no-console': [
+      'error',
+      {
+        allow: ['tron'],
+      },
+    ],
+    'react/state-in-constructor': 'off',
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
-  }
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
